ac3b0a641251c3662c72678cbca35b4ac982e573
Marcelo Gassen <marcelo.gassen@aawz.com.br>
2025-10-05 19:10:54 -0300

    fix: remove not null from composicao_apresentacao_medicamento dosagem and unidade_medida

--- data_pipelines/dags/medication_etl_src/migrations/0013_create_table_composicao_apresentacao_medicamento.sql
+++ data_pipelines/dags/medication_etl_src/migrations/0013_create_table_composicao_apresentacao_medicamento.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS composicao_apresentacao_medicamento
 (
  id_principio_ativo          uuid NOT NULL,
  id_apresentacao_medicamento uuid NOT NULL,
- dosagem                     decimal NOT NULL,
- unidade_de_medida           varchar NOT NULL,
+ dosagem                     decimal,
+ unidade_de_medida           varchar,
  CONSTRAINT PK_17 PRIMARY KEY ( id_principio_ativo, id_apresentacao_medicamento ),
  CONSTRAINT FK_6 FOREIGN KEY ( id_principio_ativo ) REFERENCES principio_ativo ( id_principio_ativo ),
  CONSTRAINT FK_7 FOREIGN KEY ( id_apresentacao_medicamento ) REFERENCES apresentacao_medicamento ( id_apresentacao_medicamento )
