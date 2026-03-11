b2bb4fe232cf2c7d2063e0eff30cc186d17d624d
Marcelo Gassen <marcelo.gassen@aawz.com.br>
2026-03-10 23:25:15 -0300

    fix: migration apresentacao medicmaneto

--- data_pipelines/dags/medication_etl_src/migrations/0018_create_table_forma_farmaceutica_apresentacao_medicamento.sql
+++ data_pipelines/dags/medication_etl_src/migrations/0018_create_table_forma_farmaceutica_apresentacao_medicamento.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS forma_farmaceutica_apresentacao_medicamento
 (
  id_apresentacao_medicamento uuid NOT NULL,
  id_forma_farmaceutica       uuid NOT NULL,
- CONSTRAINT PK_18 PRIMARY KEY ( id_apresentacao_medicamento, id_forma_farmaceutica ),
- CONSTRAINT FK_18_1 FOREIGN KEY ( id_apresentacao_medicamento ) REFERENCES apresentacao_medicamento ( id_apresentacao_medicamento ) ON DELETE CASCADE,
- CONSTRAINT FK_18_2 FOREIGN KEY ( id_forma_farmaceutica ) REFERENCES forma_farmaceutica ( id_forma_farmaceutica ) ON DELETE CASCADE
+ CONSTRAINT PK_19 PRIMARY KEY ( id_apresentacao_medicamento, id_forma_farmaceutica ),
+ CONSTRAINT FK_19_1 FOREIGN KEY ( id_apresentacao_medicamento ) REFERENCES apresentacao_medicamento ( id_apresentacao_medicamento ) ON DELETE CASCADE,
+ CONSTRAINT FK_19_2 FOREIGN KEY ( id_forma_farmaceutica ) REFERENCES forma_farmaceutica ( id_forma_farmaceutica ) ON DELETE CASCADE
 );
 CREATE INDEX IF NOT EXISTS idx_forma_farmaceutica_apresentacao_id_apresentacao ON forma_farmaceutica_apresentacao_medicamento ( id_apresentacao_medicamento );
